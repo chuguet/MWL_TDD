9c30fe925ffd99a59fd57e1abb9c404bd0ba1e45
Huguet <huguet10@gmail.com>
2015-04-08 20:14:28 +0200

    Primera subida con lo copiado en clase

---
+++ src/tests/MoveCardControllerTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.Test;
+
+public class MoveCardControllerTest {
+
+	@Test
+	public void test() {
+	}
+
+}
---
+++ src/tests/StartGameControllerTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.Test;
+
+public class StartGameControllerTest {
+
+	@Test
+	public void test() {
+	}
+
+}
