439b89895139b9261ea17f8b9564630ef23a6ea9
dmRoberto <roberto.d20@gmail.com>
2015-04-16 17:59:55 +0200

    Refactorizacion historia de usuario startGame

--- src/controllers/StartGameController.java
+++ src/controllers/StartGameController.java
@@ -1,49 +1,105 @@
 package controllers;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 import models.Card;
+import models.CardStack;
+import models.CardSuit;
+import models.DeckCardStack;
+import models.FoundationCardStack;
+import models.TableauCardStack;
+import models.WasteCardStack;
 
 public class StartGameController {
-    public int sizeWaste() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
 
-    public ArrayList<Integer> sizeFoundations() {
-        ArrayList<Integer> sizeFoundations = new ArrayList<Integer>();
-        for (int i = 0; i < 4; i++) {
-            sizeFoundations.add(0);
-        }
+	private static final int FOUNDATIONS = 4;
+	private static final int TABLEAUS = 7;
 
-        return sizeFoundations;
-    }
+	private ArrayList<CardStack> foundationsList;
+	private ArrayList<CardStack> tableausList;
+	private CardStack deck;
+	private CardStack waste;
 
-    public int sizeDeck() {
-        return 24;
-    }
+	public StartGameController() {
+		deck = new DeckCardStack();
+		initDeck();
+		waste = new WasteCardStack();
 
-    public ArrayList<Integer> sizeCoveredCardsTableaus() {
-    	ArrayList <Integer> tableaus = new ArrayList<Integer> ();
-    	for(int i = 0; i<7;i++){
-    		tableaus.add(new Integer(i+1));
-    	}
-        return tableaus;
-    }
+		tableausList = new ArrayList<CardStack>(TABLEAUS);
+		foundationsList = new ArrayList<CardStack>(FOUNDATIONS);
 
-    public ArrayList<Stack<Card>> uncoveredCardsStackTableaus() {
-    	ArrayList <Stack<Card>> uncoveredTableaus = new ArrayList<Stack<Card>> ();
-    	for(int i = 0; i<7;i++){
-    		Stack<Card> tableauUncovered = new Stack<Card>();
-    		Card uncoveredCard = new Card();
-    		uncoveredCard.setUncovered(true);
-    		tableauUncovered.add(uncoveredCard);
-    		uncoveredTableaus.add(tableauUncovered);
-    	}
-        return uncoveredTableaus;
-    }
-    
-    
+		for (int i = 0; i < TABLEAUS; i++) {
+			tableausList.add(initTableau(i));
+		}
+
+		for (int j = 0; j < FOUNDATIONS; j++) {
+			foundationsList.add(new FoundationCardStack());
+		}
+	}
+
+	public int sizeWaste() {
+		return waste.size();
+	}
+
+	public int sizeDeck() {
+		return deck.size();
+	}
+
+	public ArrayList<Integer> sizeFoundations() {
+		ArrayList<Integer> sizeFoundations = new ArrayList<Integer>();
+		for (int i = 0; i < FOUNDATIONS; i++) {
+			sizeFoundations.add(foundationsList.get(i).size());
+		}
+
+		return sizeFoundations;
+	}
+
+	public ArrayList<Integer> sizeCoveredCardsTableaus() {
+		ArrayList<Integer> tableaus = new ArrayList<Integer>();
+
+		for (CardStack tableau : tableausList) {
+			tableaus.add(tableau.size());
+		}
+
+		return tableaus;
+	}
+
+	public ArrayList<Stack<Card>> uncoveredCardsStackTableaus() {
+		ArrayList<Stack<Card>> uncoveredTableaus = new ArrayList<Stack<Card>>();
+		for (CardStack tableau : tableausList) {
+			Stack<Card> tableauUncovered = new Stack<Card>();
+			tableauUncovered.add(tableau.peek());
+			uncoveredTableaus.add(tableauUncovered);
+		}
+
+		return uncoveredTableaus;
+	}
+
+	private void initDeck() {
+		ArrayList<Card> temporalDeck = new ArrayList<Card>();
+		for (CardSuit suit : CardSuit.values()) {
+			for (int i = 1; i <= 13; i++) {
+				temporalDeck.add(new Card(suit, i, false));
+			}
+		}
+		while (!temporalDeck.isEmpty()) {
+			int randomIndex = (int) (Math.random() * (temporalDeck.size() - 1));
+			deck.push(temporalDeck.get(randomIndex));
+			temporalDeck.remove(randomIndex);
+		}
+	}
+
+	private CardStack initTableau(int order) {
+		CardStack tableau = new TableauCardStack();
+		for (int i = 0; i <= order; i++) {
+			Card card = deck.pop();
+			if (i == order) {
+				card.setUncovered(true);
+			}
+			tableau.push(card);
+		}
+		return tableau;
+	}
+
 }
--- src/models/DeckCardStack.java
+++ src/models/DeckCardStack.java
@@ -1,6 +1,8 @@
 package models;
 
-public class DeckCardStack extends CardStack{
+public class DeckCardStack extends CardStack {
+
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public boolean pushAllowed(Card card) {
